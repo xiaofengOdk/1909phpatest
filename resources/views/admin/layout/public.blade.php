1a5c816ecdd9b4a06ef164c426f0203717f13717
xinghuifeng <2574047739@qq.com>
2020-07-22 14:43:50 +0800

    xhf

--- resources/views/admin/layout/public.blade.php
+++ resources/views/admin/layout/public.blade.php
@@ -399,6 +399,25 @@
                         </ul>
                     </li>
 
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-folder"></i>
+                            <span>分类管理</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+
+                            <li id="admin-login">
+                                <a href="/admin/category" target="iframe">
+                                    <i class="fa fa-circle-o"></i>分类管理
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+
                     <!-- 菜单 /-->
 
                 </ul>

96d534082f70e42935e8750f644f9258c020e3e6
liu <2938937056@qq.com>
2020-07-22 14:57:02 +0800

    lyh

--- resources/views/admin/layout/public.blade.php
+++ resources/views/admin/layout/public.blade.php
@@ -398,7 +398,22 @@
                             </li>
                         </ul>
                     </li>
-
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-folder"></i>
+                            <span>轮播图管理</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li id="admin-login">
+                                <a href="/admin/slide_show" target="iframe">
+                                    <i class="fa fa-circle-o"></i>查看
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                     <!-- 菜单 /-->
 
                 </ul>

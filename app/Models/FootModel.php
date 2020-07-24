fe41888a66c8ddc00cb4560f469db9fbc0bd44d1
xiaofei <2924948488@qq.com>
2020-07-24 11:59:28 +0800

    foot

---
+++ app/Models/FootModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FootModel extends Model
+{
+    protected $table='foot';
+    protected $primaryKey='id';
+    public $timestamps=false;
+}

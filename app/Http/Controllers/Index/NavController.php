5a4205a0bb49045376f1d6ad4dafeb2a53f335b7
liu <2938937056@qq.com>
2020-07-27 19:16:12 +0800

    xx

--- app/Http/Controllers/Index/NavController.php
+++ app/Http/Controllers/Index/NavController.php
@@ -11,6 +11,7 @@ use App\Models\BrandModel;
 use App\Models\FootModel;
 class NavController extends Controller
 {
+    // top
     public function nav_list($id){
         $nav = NavModel::get();//导航
         $brand = BrandModel::where("brand_show",1)->get();//热卖
@@ -18,5 +19,13 @@ class NavController extends Controller
         $navgoods = Goods::where("nav_id",$id)->get();
         // dd($navgoods);
         return view("index/nav/nav_list",compact("nav","brand","footInfo","navgoods"));
+    }
+    // top
+    public function nav_hot($id){
+        $nav = NavModel::get();//导航
+        $brand = BrandModel::where("brand_show",1)->get();//热卖
+        $footInfo=FootModel::get();
+        $hot = Goods::where("cate_id",$id)->get();//热卖商品
+        return view("index/nav/nav_hot",compact("nav","brand","footInfo","hot"));
     }
 }

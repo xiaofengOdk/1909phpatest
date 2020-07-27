fdf2cae6eba9b5734e8cf666ee7a83bb23894514
xiaofei <2924948488@qq.com>
2020-07-27 14:22:04 +0800

    file

---
+++ app/Http/Controllers/Index/GoodsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Index;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\NavModel;
+use App\Models\FootModel;
+use App\Models\BrandModel;
+
+class GoodsController extends Controller
+{
+    /**
+     * 商品列表
+     */
+    public function goods_list($id){
+//        echo $id;
+        $nav = NavModel::get();//导航
+        $footInfo=FootModel::get();//底部导航
+        $brand = BrandModel::where("brand_show",1)->get();//热卖
+        $brandInfo = BrandModel::where('cate_id',$id)->get();
+        return view('index/goods/goodslist',["nav"=>$nav,"brand"=>$brand,"footInfo"=>$footInfo,'brandInfo'=>$brandInfo]);
+    }
+}

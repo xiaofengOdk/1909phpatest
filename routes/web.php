0c67f65e97a7198b301426973272f681f7145444
xinghuifeng <2574047739@qq.com>
2020-07-28 11:53:03 +0800

    xhf

--- routes/web.php
+++ routes/web.php
@@ -146,4 +146,5 @@ Route::prefix("/index")->group(function(){
 });
 Route::prefix("/user")->group(function(){
     Route::any('/user_info','Index\UserController@user_info');
+    Route::any('/user_add','Index\UserController@user_add');
 });

654c738450046acb8b2567c4337258efe9cfdf13
liu <2938937056@qq.com>
2020-08-04 19:41:07 +0800

    sc

--- app/Http/Controllers/Index/CartshopController.php
+++ app/Http/Controllers/Index/CartshopController.php
@@ -15,83 +15,92 @@ class CartshopController extends Controller
     public function cart_add(Request $request){
         $goods_id = $request->goods_id;
         $buy_number = $request->buy_number;
-        // dd($goods_id);
+//        dd($buy_number);
         $reg = session("reg");
+//        dd($reg);
         $user_id = $reg['user_id'];
         if($user_id==null){
-            $cartdata = cookie("cartdata");
-            // $buy_number = 1;
-            $goods = Goods::where("goods_id",$goods_id)->first();
-            // 判断库存
-            if($goods['goods_store']<$buy_number){
-                return $message=[
-                        "code"=>00001,
-                        "message"=>"库存不足",
-                        "success"=>false,
-                    ];
-            }
-            $id=request()->goods_id;
-            // dd($id);
-            // $id=implode("",$id);
-            $cookie = $request->cookie('test');
-            if(empty($cookie)){
-                $cookie = [];
-            }
-            // dd($cookie);
-            if($cookie==[]){
-                // echo 2;exit;
-                $goods_id  = request()->goods_id;
-                $cartdata=[];
-                $cartdata = [
-                    "goods_id"=>$request->goods_id,
-                    "buy_number"=>$buy_number,
-                    "add_time"=>time(),
-                ];
-                $cartdata["goods_id_".$goods_id]=$goods_id;
-                $cartdata=json_encode($cartdata);
-            }else{
-                // echo 1;
-                $cookie=json_decode($cookie);
-            if(array_key_exists("goods_id_".$id,$cookie)){
-                // dd(1);
+                $cartdata = cookie("cartdata");
+                // $buy_number = 1;
+                $goods = Goods::where("goods_id",$goods_id)->first();
+                // 判断库存
                 if($goods['goods_store']<$buy_number){
-                    $carts = $goods['goods_store'];
-                }else{
-                    // dd($cookie->buy_number+$buy_number);
-                    $cart = $cookie->buy_number+$buy_number;
+                    return $message=[
+                            "code"=>00001,
+                            "message"=>"库存不足",
+                            "success"=>false,
+                        ];
                 }
-                $cartdata = [
-                    "buy_number"=>$cart,
-                    "goods_id"=>$id,
-                    "add_time"=>time(),
-                ];
-                $cartdata["goods_id_".$id]=$id;
-                // dd($);
-                $cartdata=json_encode($cartdata);
-            }else{
-                // dd(2);
-                $goods_id  = request()->goods_id;
-                $cartdata=[];
-                $cartdata = [
-                    "goods_id"=>$request->goods_id,
-                    "buy_number"=>$buy_number,
-                    "add_time"=>time(),
-                ];
-                $cartdata["goods_id_".$goods_id]=$goods_id;
-                $cartdata=json_encode($cartdata);
-             }
-            }
-            // dd($cartdata);         
-            Cookie::queue("test",$cartdata);
-           
-                return $message=[
-                    "code"=>00001,
-                    "message"=>"已加入购物车",
-                    "success"=>true,
-                ];
+
+                $id=request()->goods_id;
+                // dd($id);
+                // $id=implode("",$id);
+                $cookie = $request->cookie('test');
+                if(empty($cookie)){
+                    $cookie = [];
+                }
+                // dd($cookie);
+                if($cookie==[]){
+                    // echo 2;exit;
+                    $goods_id  = request()->goods_id;
+                    $cartdata=[];
+                    $cartdata[$goods_id] = [
+                        "goods_id"=>$request->goods_id,
+                        "buy_number"=>$buy_number,
+                        "add_time"=>time(),
+                    ];
+                    $cartdata[$goods_id]["goods_id_".$goods_id]=$goods_id;
+                    $cartdata=json_encode($cartdata);
+                }else{
+                    // echo 1;
+                    $cookie=json_decode($cookie);
+//                if(array_key_exists("goods_id_".$id,$cookie)){
+                if(array_key_exists($id,$cookie)){
+                    // dd(1);
+                    if($goods['goods_store']<$buy_number){
+                        $carts = $goods['goods_store'];
+                    }else{
+                        // dd($cookie->buy_number+$buy_number);
+                        $cart = $cookie->buy_number+$buy_number;
+                        #当购买数量相加时超过库存时
+                        if($goods['goods_store']<$cart){
+                            return [
+                                "code"=>00002,
+                                "message"=>"库存不足2",
+                                "success"=>false,
+                            ];
+                        }
+                    }
+                    $cartdata[$goods_id] = [
+                        "buy_number"=>$cart,
+                        "goods_id"=>$id,
+                        "add_time"=>time(),
+                    ];
+                    $cartdata[$goods_id]["goods_id_".$id]=$id;
+                    // dd($);
+                    $cartdata=json_encode($cartdata);
+                }else{
+                    // dd(2);
+                    $goods_id  = request()->goods_id;
+                    $cartdata=[];
+                    $cartdata[$goods_id] = [
+                        "goods_id"=>$request->goods_id,
+                        "buy_number"=>$buy_number,
+                        "add_time"=>time(),
+                    ];
+                    $cartdata[$goods_id]["goods_id_".$goods_id]=$goods_id;
+                    $cartdata=json_encode($cartdata);
+                 }
+                }
+                //  dd($cartdata);
+                Cookie::queue("test",$cartdata);
+                    return $message=[
+                        "code"=>00001,
+                        "message"=>"已加入购物车",
+                        "success"=>true,
+                    ];
 
         }else{
-           
             // 登录后
             // $buy_number = 1;
             $goods = Goods::where("goods_id",$goods_id)->first();
--- resources/views/index/layout/left.blade.php
+++ resources/views/index/layout/left.blade.php
@@ -14,24 +14,24 @@
                         <dl>
 							<dt><i>·</i> 订单中心</dt>
 							<dd ><a href="{{url('/index/user_index')}}" >我的订单</a></dd>
-							<dd><a href="home-order-pay.html" >待付款</a></dd>
-							<dd><a href="home-order-send.html"  >待发货</a></dd>
-							<dd><a href="home-order-receive.html" >待收货</a></dd>
-							<dd><a href="home-order-evaluate.html" >待评价</a></dd>
+							<dd><a href="home-order-pay.html" ></a></dd>
+							<dd><a href="home-order-send.html"  ></a></dd>
+							<dd><a href="home-order-receive.html" ></a></dd>
+							<dd><a href="home-order-evaluate.html" ></a></dd>
 						</dl>
 						<dl>
-							<dt><i>·</i> 我的中心</dt>
-							<dd><a href="home-person-collect.html">我的收藏</a></dd>
-							<dd><a href="home-person-footmark.html">我的足迹</a></dd>
+							<dt><i>·</i> </dt>
+							<dd><a href="home-person-collect.html"></a></dd>
+							<dd><a href="home-person-footmark.html"></a></dd>
 						</dl>
 						<dl>
-							<dt><i>·</i> 物流消息</dt>
+							<dt><i>·</i> </dt>
 						</dl>
 						<dl>
-							<dt><i>·</i> 设置</dt>
+							<dt><i></i> 设置</dt>
 							<dd><a href="{{url('/user/user_info')}}">个人信息</a></dd>
 							<dd><a href="{{url('user/user_address')}}"  >地址管理</a></dd>
-							<dd><a href="home-setting-safe.html" >安全管理</a></dd>
+							
 						</dl>
                     </div>
                 </div>

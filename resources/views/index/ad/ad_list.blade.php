5154525bea2740391a310adf9a6ecfe0b0053b6a
xiaoxiao <2356911868@qq.com>
2020-08-06 21:11:06 +0800

    ds

--- resources/views/index/ad/ad_list.blade.php
+++ resources/views/index/ad/ad_list.blade.php
@@ -67,7 +67,7 @@
 		  艾蒂妲快报 &nbsp;:&nbsp;
 		  <b style="color:#b1191a; font-size:15px;">{{$reg->g_name}}</b>
 	  </p>
-      <div style="border:1px solid #ccc; width:1200px; height:450px; background:#fff;margin:0 auto;">
+      <div style="border:1px solid #ccc; width:1200px; height:500px; background:#fff;margin:0 auto;">
           <h2 style="text-align:center; margin-top:30px;">{{$reg->g_name}}</h2>
          <div align="center"> <p style="text-align:left; margin-top:20px; width:600px;   font-size: 13px; text-indent:2em;">{{$reg->g_desc}}</p></div>
 		  {{--图片--}}
